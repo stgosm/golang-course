6129ca90911371bc2086a30e1e840bc4a2c0ab3e
stgosm-nuxiba <sserrano@nuxiba.com>
2021-06-26 00:06:34 -0500

    Funciones

--- src/main.go
+++ src/main.go
@@ -2,26 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	// https://golang.org/pkg/fmt/
-	// Declaración de variables
-	helloMessage := "Hello"
-	worldMessage := "World"
-
-	// Println
-	fmt.Println(helloMessage, worldMessage)
-
-	// Printf
-	nombre := "Platzi"
-	cursos := 500
-	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
-	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
-
-	// Sprintf
-	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
+func normalFunction(message string) {
 	fmt.Println(message)
+}
 
-	// Tipo de datos
-	fmt.Printf("helloMessage: %T\n", helloMessage)
-	fmt.Printf("cursos: %T", cursos)
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	normalFunction("Hello World")
+	tripleArgument(1, 2, "Hello")
+
+	value := returnValue(2)
+	fmt.Println("value:", value)
+
+	value1, _ := doubleReturn(2)
+	fmt.Println("value1:", value1)
 }
