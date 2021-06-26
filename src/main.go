0852dcd51b1341223a43a602638ce4d89d83a1a2
stgosm-nuxiba <sserrano@nuxiba.com>
2021-06-25 23:36:14 -0500

    Conceptos básicos

--- src/main.go
+++ src/main.go
@@ -3,7 +3,61 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
 
-	fmt.Println("Hello world, this is my second go code")
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
 
+	// Declaración de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero value
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	// División
+	result = y / x
+	fmt.Println("División:", result)
+
+	// Módulo
+	result = y % x
+	fmt.Println("Módulo:", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental:", x)
 }
